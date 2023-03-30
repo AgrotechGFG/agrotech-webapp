124ce119d4c7132066a39370b82e499b830cac8a
Bhanu Prakash <110401986+BhanuPrakash7297@users.noreply.github.com>
2023-03-30 14:33:44 +0530

    Update App.js
    deleted comment out import files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-// import Navbar from './components/Navbar/Navbar';
-import Button from './components/buttons/Button';
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
-import AboutUs from './pages/AboutUs';
-import Services from './pages/Services';
-import Products from './pages/Products';
-import News from './pages/News';
-import Blog from './pages/Blog';
+
 import Home from './pages/Home';
 function App() {
   return (
